a64b9472b3783606c845d283f1bd0fca80e86d05
Robin <robin.avoort@gmail.com>
2015-04-02 13:52:20 +0200

    Edited files for Domain Model

--- MoC/src/domain/NewsFeed.java
+++ MoC/src/domain/NewsFeed.java
@@ -2,53 +2,42 @@ package domain;
 
 //@Author Casper
 
-import domain.Events.NewsItemPublishedEvent;
-import domain.Events.NewsItemPublishedListener;
+import domain.Events.HintReleasedEvent;
+import domain.Events.HintReleasedListener;
+import domain.Events.RoundEndedEvent;
 import domain.Events.RoundEndedListener;
-import javax.swing.event.EventListenerList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class NewsFeed {
+public class NewsFeed implements HintReleasedListener, RoundEndedListener{
 
-    protected EventListenerList newsItemPublishedListenerList;
+    private List<NewsItem> newsItems;
     
     //should probably be a singleton
     public NewsFeed(){
-        this.newsItemPublishedListenerList = new EventListenerList();
+        this.newsItems = new ArrayList<>();
+    }
+
+    public List<NewsItem> getNewsItems() {
+        return newsItems;
+    }
+    
+    public void addNewsItem(NewsItem newsItem){
+        newsItems.add(newsItem);
     }
     
     public void publishNewsItem(NewsItem toPublish){
         toPublish.setTimestamp("current time");
         //throw event
     }
-    
-    /**
-     * Adds the specified NewsItemPublishedListener to the listener list.
-     *
-     * @param toAdd The NewsItemPublishedListener to add.
-     */
-    public void addRoundEndedListener(NewsItemPublishedListener toAdd) {
-        newsItemPublishedListenerList.add(NewsItemPublishedListener.class, toAdd);
+
+    @Override
+    public void hintReleasedOccurred(HintReleasedEvent event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    /**
-     * Removes the specified NewsItemPublishedListener from the listener list.
-     *
-     * @param toRemove The NewsItemPublishedListener to remove.
-     */
-    public void removeRoundEndedListener(NewsItemPublishedListener toRemove) {
-        newsItemPublishedListenerList.remove(NewsItemPublishedListener.class, toRemove);
-    }
-
-    /**
-     * Fires the news item published event.
-     * @param toFire The event to fire.
-     */
-    private void fireRoundEndedEvent(NewsItemPublishedEvent toFire) {
-        Object[] listeners = newsItemPublishedListenerList.getListenerList();
-        for (int i = 0; i < listeners.length; i = i + 2) {
-            if (listeners[i] == NewsItemPublishedListener.class) {
-                ((NewsItemPublishedListener) listeners[i + 1]).newsItemPublishedOccurred(toFire);
-            }
-        }
+    @Override
+    public void roundEndedOccurred(RoundEndedEvent event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
