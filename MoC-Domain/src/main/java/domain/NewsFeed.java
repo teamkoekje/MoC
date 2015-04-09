7d0589375c8e4dcb0476b0053f27f34aa72877cb
Astrid Belder <a.belder@student.fontys.nl>
2015-04-09 10:41:15 +0200

    Conflict in NewsFeed.java fixed

--- MoC-Domain/src/main/java/domain/NewsFeed.java
+++ MoC-Domain/src/main/java/domain/NewsFeed.java
@@ -1,49 +1,28 @@
 package domain;
 
 //@Author Casper
-<<<<<<< HEAD
 import domain.Events.NewsItemPublishedEvent;
 import domain.Events.NewsItemPublishedListener;
-=======
 
 import domain.Events.HintReleasedEvent;
 import domain.Events.HintReleasedListener;
 import domain.Events.RoundEndedEvent;
->>>>>>> 7f8809cb67711473af09c09a45fc612ae34b720f
 import domain.Events.RoundEndedListener;
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.event.EventListenerList;
 
-public class NewsFeed implements HintReleasedListener, RoundEndedListener{
+public class NewsFeed implements HintReleasedListener, RoundEndedListener {
 
-<<<<<<< HEAD
     protected EventListenerList newsItemPublishedListenerList;
 
     //should probably be a singleton
     public NewsFeed() {
         this.newsItemPublishedListenerList = new EventListenerList();
-=======
-    private List<NewsItem> newsItems;
-    
-    //should probably be a singleton
-    public NewsFeed(){
-        this.newsItems = new ArrayList<>();
-    }
-
-    public List<NewsItem> getNewsItems() {
-        return newsItems;
-    }
-    
-    public void addNewsItem(NewsItem newsItem){
-        newsItems.add(newsItem);
->>>>>>> 7f8809cb67711473af09c09a45fc612ae34b720f
     }
 
     public void publishNewsItem(NewsItem toPublish) {
         toPublish.setTimestamp("current time");
         //throw event
     }
-<<<<<<< HEAD
 
     /**
      * Adds the specified NewsItemPublishedListener to the listener list.
@@ -53,15 +32,12 @@ public class NewsFeed implements HintReleasedListener, RoundEndedListener{
     public void addRoundEndedListener(NewsItemPublishedListener toAdd) {
         newsItemPublishedListenerList.add(NewsItemPublishedListener.class, toAdd);
     }
-=======
->>>>>>> 7f8809cb67711473af09c09a45fc612ae34b720f
 
     @Override
     public void hintReleasedOccurred(HintReleasedEvent event) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-<<<<<<< HEAD
     /**
      * Fires the news item published event.
      *
@@ -74,10 +50,10 @@ public class NewsFeed implements HintReleasedListener, RoundEndedListener{
                 ((NewsItemPublishedListener) listeners[i + 1]).newsItemPublishedOccurred(toFire);
             }
         }
-=======
+    }
+
     @Override
     public void roundEndedOccurred(RoundEndedEvent event) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
->>>>>>> 7f8809cb67711473af09c09a45fc612ae34b720f
     }
 }
