0b773facf54f1f14300fc0b33847daa23dad2023
daangoumans <daan.goumans@hotmail.com>
2015-04-16 15:43:06 +0200

    Barry Commit
    Barry kan alles

---
+++ barry2/src/test/java/barry2Test.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Daan
+ */
+public class barry2Test {
+    
+    public barry2Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class barry2.
+     */
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+    
+    @Test
+    public void test2(){
+        assertTrue(false);
+    }
+    
+}
