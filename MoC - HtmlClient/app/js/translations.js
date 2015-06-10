70da48a1de69e5c9cb40e7a631cce3add5ec7bb2
I270563 <arno.schellekens@student.fontys.nl>
2015-06-10 13:24:31 +0200

    Internationalization toegepast en flag images toegevoegd :fire:

--- MoC - HtmlClient/app/js/translations.js
+++ MoC - HtmlClient/app/js/translations.js
@@ -1,5 +1,11 @@
 var translationsEN = {
   USERNAME: 'Username',
+  NEW_USER: 'New user',
+  EXISTING_USER: 'Existing user',
+  INVALID_USER: 'Invalid username',
+  
+  BACK: 'Back',
+  NEW: 'New',
   PASSWORD: 'Password',
   REGISTER: 'Regsiter',
   LOGIN: 'Log in',
@@ -8,17 +14,37 @@ var translationsEN = {
   COMPETITION: 'Competition',
   COMPETITIONS: 'Competitions',
   
+  INVITE: 'Invite',
+  INVITATIONS: 'Invitations',
+  NO_INVITE: 'You are currently not invited to any teams.',
+  
+  ORGANISATION: 'Organisation',
+  
   MYTEAMS: 'My teams',
   TEAMS: 'Teams',
   NO_TEAMS: 'No teams have been added to this competition yet.',
   NEW_TEAM: 'Create new team',
   TEAM_NAME: 'Team name',
   CREATE_TEAM: 'Create team', 
+  ADD_TEAM_MEMBER: 'Add team member',
+  ADD_NEW_TEAM_MEMBER: 'Add new team member',
+  TEAM_INFO: 'Team information',
+  TEAM_OWNER: 'Team owner',
+  TEAM_MEMBER: 'Team members',
   
   OWNER: 'Owner',
   POINTS: 'Points',
   NAME: 'Name',
   DATE: 'Date',
+  SUCCES: 'Succes',
+  OOPS: 'Oops',
+  STATUS: 'Status',
+  
+  EMAIL_SEND: 'An email has been sent, the user has been invited to the team.',
+  EMAIL_ERROR: 'Something went wrong when trying to invite the user.',
+  EMAIL_SUCCES_REGISTER: 'An email has been sent, click on the link your mail to activate your account.',
+  EMAIL_ERROR_REGISTER: 'Something went wrong when trying to register your account.',
+  
   DIFFICULTY: 'Difficulty',
   STARTS_AT: 'Starts at',
   START_CODING: 'Start coding!',
@@ -34,12 +60,21 @@ var translationsEN = {
   CHALLENGES: 'Challenges',
   NO_CHALLENGE: 'No challenges have been added to this competition yet.',
   
+  INVITE_USERS: 'Invite users',
+  SEARCH_USERS: 'Search user',
+  
   BUTTON_LANG_NL: 'Dutch',
   BUTTON_LANG_EN: 'English'
 };
 
 var translationsNL= {
   USERNAME: 'Gebruikersnaam',
+  NEW_USER: 'Nieuwe gebruiker',
+  EXISTING_USER: 'Bestaande gebruiker',
+  INVALID_USER: 'Gebruikersnaam ongeldig',
+  
+  BACK: 'Terug',
+  NEW: 'Nieuw',
   PASSWORD: 'Wachtwoord',
   REGISTER: 'Registeer',
   LOGIN: 'Aanmelden',
@@ -48,17 +83,37 @@ var translationsNL= {
   COMPETITION: 'Competitie',
   COMPETITIONS: 'Competities',
   
+  INVITE: 'Uitnodigen',
+  INVITATIONS: 'Uitnodigingen',
+  NO_INVITE: 'Je bent momenteel niet door een team uitgenodigd.',
+  
+  ORGANISATION: 'Organisatie',
+  
   MYTEAMS: 'Mijn teams',
   TEAMS: 'Teams',
   NO_TEAMS: 'Er zijn nog geen teams toegevoegd aan de competitie.',
   NEW_TEAM: 'Nieuw team aanmaken',
   TEAM_NAME: 'Team naam',
-  CREATE_TEAM: 'Team aanmaken', 
+  CREATE_TEAM: 'Team aanmaken',
+  ADD_TEAM_MEMBER: 'Team lid toevoegen',
+  ADD_NEW_TEAM_MEMBER: 'Nieuw team lid toevoegen',
+  TEAM_INFO: 'Team informatie',
+  TEAM_OWNER: 'Team eigenaar',
+  TEAM_MEMBER: 'Teamleden',
   
   OWNER: 'Eigenaar',
   POINTS: 'Punten',
   NAME: 'Naam',
   DATE: 'Datum',
+  SUCCES: 'Success',
+  OOPS: 'Oeps',
+  STATUS: 'Status',
+  
+  EMAIL_SEND: 'Een email is verstuurd, de gebruiker is uitgenodigd tot het team.',
+  EMAIL_ERROR: 'Tijdens het uitnodingen van de gebruiker ging er iets fout.',
+  EMAIL_SUCCES_REGISTER: 'Een email is verstuurd, klik op de link in de mail om je account te registreren.',
+  EMAIL_ERROR_REGISTER: 'Er ging iets fout tijdens het registeren van je account.',
+  
   DIFFICULTY: 'Moeilijkheidsgraad',
   STARTS_AT: 'Begind om',
   START_CODING: 'Begin coderen!',
@@ -73,6 +128,9 @@ var translationsNL= {
   
   CHALLENGES: 'Opdrachten',
   NO_CHALLENGE: 'Er geen geen opdrachten toegevoegd aan de competitie.',
+  
+  INVITE_USERS: 'Nodig gebruiker uit',
+  SEARCH_USERS: 'Zoek gebruiker',
   
   BUTTON_LANG_NL: 'Nederlands',
   BUTTON_LANG_EN: 'Engels'
