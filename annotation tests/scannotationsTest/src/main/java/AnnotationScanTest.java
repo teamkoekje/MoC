216ba4f4c5cb102b22e0e6c5926192eda45cb659
Casper <c.linschooten@gmail.com>
2015-06-16 11:03:58 +0200

    Improved AnnotationScanTest to include scanning available tests, and values of annotations.

--- annotation tests/scannotationsTest/src/main/java/AnnotationScanTest.java
+++ annotation tests/scannotationsTest/src/main/java/AnnotationScanTest.java
@@ -2,18 +2,27 @@
 import annotations.*;
 import java.io.File;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import org.scannotation.*;
+import org.testng.annotations.Test;
+import projectwithannotations.annotatedproject.challenge.ChallengeExample;
 
 /**
  * @author Casper
@@ -21,43 +30,140 @@ import org.scannotation.*;
 public class AnnotationScanTest {
 
     private static List<String> visibleClasses;
-    private static AnnotationDB db = new AnnotationDB();
-    
-    private static List<String> editables;
+    private static List<String> editableClasses;
+    private static final AnnotationDB db = new AnnotationDB();
+
+    private static final List<Challenge> challenges = new ArrayList<>();
+    private static final List<Test> userTests = new ArrayList<>();
+    private static final List<Test> systemTests = new ArrayList<>();
+    private static final List<Test> ambivalentTests = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
-            //the jar path
-            URL testJarUrl = new URL("file:///" + System.getProperty("user.dir") + "\\src\\main\\java\\resources\\annotatedProject-1.0.jar");
-            System.out.println("Using the following jar to determine the annotations:\n" + testJarUrl.getPath());
-            //setup scanner & scan
-
-            db.scanArchives(testJarUrl);
+            //<editor-fold defaultstate="collapsed" desc="jar paths">
+            String resourcePath = "file:///" + System.getProperty("user.dir") + "\\src\\main\\java\\resources\\";
+            URL testJarUrl = new URL(resourcePath + "annotatedProject-1.0.jar");
+            URL testJarUrl2 = new URL(resourcePath + "annotatedProject-1.0-tests.jar");
+            URL[] urls = new URL[2];
+            urls[0] = testJarUrl;
+            urls[1] = testJarUrl2;
+            System.out.println("Using the following jars to determine the annotated classes:\n" + testJarUrl.getPath() + "\n" + testJarUrl2.getPath());
+            //</editor-fold>
+            //<editor-fold defaultstate="collapsed" desc="scan">
+            db.scanArchives(urls);
             Map<String, Set<String>> annotationIndex = db.getAnnotationIndex();
             visibleClasses = new ArrayList<>();
-            //iterate over results
+            //<editor-fold defaultstate="collapsed" desc="visible classes">
             processResults("Challenge", annotationIndex.get(Challenge.class.getName()));
             processResults("Editable", annotationIndex.get(Editable.class.getName()));
             processResults("ReadOnly", annotationIndex.get(ReadOnly.class.getName()));
             processResults("Hint", annotationIndex.get(Hint.class.getName()));
-            
-            editables = new ArrayList<>();
-            for(String s : annotationIndex.get(Editable.class.getName())){
+            // </editor-fold>
+            //<editor-fold defaultstate="collapsed" desc="editable classes">
+            editableClasses = new ArrayList<>();
+            for (String s : annotationIndex.get(Editable.class.getName())) {
                 String[] parts = s.split("\\.");
                 String lastPart = parts[parts.length - 1];
-                editables.add(lastPart);
+                editableClasses.add(lastPart);
             }
-            
-            
+            //</editor-fold>        
+            //<editor-fold defaultstate="collapsed" desc="scan challenges">        
+            System.out.println();
+            System.out.println("______SCANNING FOR CHALLENGES______");
+            String path = System.getProperty("user.dir") + "\\src\\main\\java\\resources\\annotatedProject-1.0.jar";
+            Enumeration en = new JarFile(path).entries();
+
+            URL[] jarUrl2 = {new URL("jar:file:" + path + "!/")};
+            URLClassLoader cl2 = URLClassLoader.newInstance(jarUrl2);
+
+            while (en.hasMoreElements()) {
+                JarEntry je = (JarEntry) en.nextElement();
+                if (je.isDirectory() || !je.getName().endsWith(".class")) {
+                    continue;
+                }
+                // -6 because of .class
+                String className = je.getName().substring(0, je.getName().length() - 6);
+                className = className.replace('/', '.');
+                Class c = cl2.loadClass(className);
+                System.out.println("__________________________________________");
+                System.out.println("class name: " + c.getName());
+                System.out.println("class simple name: " + c.getSimpleName());
+                Challenge chal = (Challenge) c.getAnnotation(Challenge.class);
+                if (chal != null) {
+                    System.out.println("This class has a Challenge annotation! Values:");
+                    System.out.println("Challenge name: " + chal.name());
+                    System.out.println("Author: " + chal.author());
+                    System.out.println("Organisation: " + chal.organisation());
+                    System.out.println("Weblink: " + chal.weblink());
+                    System.out.println("Difficulty: " + chal.difficulty().name());
+                    int j = 1;
+                    for (Hint h : chal.hints()) {
+                        System.out.println("Hint" + j + ": delay: " + h.delay() + " : content: " + h.content());
+                        j++;
+                    }
+                    challenges.add(chal);
+                }
+            }
+            System.out.println("__________________________________________");
+            System.out.println();
+            //</editor-fold>
+            //<editor-fold defaultstate="collapsed" desc="scan tests">   
+            System.out.println();
+            System.out.println("______SCANNING FOR TESTS______");
+            String pathToJar = System.getProperty("user.dir") + "\\src\\main\\java\\resources\\annotatedProject-1.0-tests.jar";
+            JarFile jarFile = new JarFile(pathToJar);
+            Enumeration e = jarFile.entries();
+
+            URL[] jarUrl = {new URL("jar:file:" + pathToJar + "!/")};
+            URLClassLoader cl = URLClassLoader.newInstance(jarUrl);
+
+            while (e.hasMoreElements()) {
+                JarEntry je = (JarEntry) e.nextElement();
+                if (je.isDirectory() || !je.getName().endsWith(".class")) {
+                    continue;
+                }
+                // -6 because of .class
+                String className = je.getName().substring(0, je.getName().length() - 6);
+                className = className.replace('/', '.');
+                Class c = cl.loadClass(className);
+                System.out.println("__________________________________________");
+                System.out.println("class name: " + c.getName());
+                System.out.println("class simple name: " + c.getSimpleName());
+                Test t = (Test) c.getAnnotation(Test.class);
+                if (t.groups().length == 2) {
+                    ambivalentTests.add(t);
+                } else {
+                    switch (t.groups()[0]) {
+                        case "user":
+                            userTests.add(t);
+                            break;
+                        case "system":
+                            systemTests.add(t);
+                            break;
+                        default:
+                            throw new AssertionError("unexpected value: " + t.groups()[0]);
+                    }
+                }
+                System.out.println("test name: " + t.testName());
+                System.out.println("test description: " + t.description());
+                for (String s : t.groups()) {
+                    System.out.println("-Group member: " + s);
+                }
+            }
+            System.out.println("__________________________________________");
+            // </editor-fold>
+            // </editor-fold>
+            //<editor-fold defaultstate="collapsed" desc="file structure and json return">
             //lets print out how an example project (with some annotated classes) would look like to a particiapant
             String exampleToPrintPath = System.getProperty("user.dir") + "\\src\\main\\example to print";
-            System.out.println("\nPrinting folder structure as a participant would see it:\n" + exampleToPrintPath + "\n");
+            System.out.println("\n_____FOLDER STRUCTURE AND JSON REPLY_____:\n" + exampleToPrintPath + "\n");
             listVisibleStructureForParticipant(1, new File(exampleToPrintPath));
             System.out.println(listStructureJSON(new File(exampleToPrintPath)).build());
-            
+
             System.out.println(getFileJSON(System.getProperty("user.dir") + "\\src\\main\\example to print\\src\\main\\java\\projectwithannotations\\annotatedproject\\challenge\\NonEditableClass.java"));
             System.out.println(getFileJSON(System.getProperty("user.dir") + "\\src\\main\\example to print\\src\\main\\java\\projectwithannotations\\annotatedproject\\challenge\\ChallengeImpl.java"));
-        } catch (IOException ex) {
+            //</editor-fold>
+        } catch (Exception ex) {
             System.err.println(ex.getMessage());
         }
     }
@@ -70,10 +176,10 @@ public class AnnotationScanTest {
                 visibleClasses.add(split[split.length - 1]);
                 System.out.println(s);
             }
-            System.out.println();
         } else {
             System.out.println("No classes using the annotation: " + classname + " - If there should be, does the annotation have @Retention(RetentionPolicy.RUNTIME)?");
         }
+        System.out.println();
     }
 
     private static JsonArrayBuilder listFileJSON(File file) {
@@ -128,16 +234,16 @@ public class AnnotationScanTest {
         }
         return false;
     }
-    
-    private static Boolean isFileEditable(String filePath){
+
+    private static Boolean isFileEditable(String filePath) {
         File f = new File(filePath);
         String filename = f.getName().substring(0, f.getName().length() - 5);
-        return editables.contains(filename);
+        return editableClasses.contains(filename);
     }
 
     public static String getFileJSON(String filePath) throws IOException {
         JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
-        
+
         Path file = Paths.get(filePath);
         String filecontent = new String(Files.readAllBytes(file));
 
