af618cd79d47b5027edc94084f5e15ae2567faf8
Astrid <a.belder@student.fontys.nl>
2015-06-16 10:59:20 +0200

    Changed compile and test requests

--- MoC-Messaging/src/main/java/workspace/Requests/TestRequest.java
+++ MoC-Messaging/src/main/java/workspace/Requests/TestRequest.java
@@ -8,38 +8,38 @@ package workspace.Requests;
  */
 public class TestRequest extends TeamRequest {
 
-    private String challengeName;
-    private String testFile;
-    private String testName;
+    private final String challengeName;
+    private final String testFile;
+    private final String testName;
+    private final String filePath;
+    private final String fileContent;
 
-    public TestRequest(long competitionId, String teamname, String challengeName, String testFile, String testName) {
+    public TestRequest(long competitionId, String teamname, String challengeName, String testFile, String testName, String filePath, String fileContent) {
         super(RequestAction.TEST, competitionId, teamname);
         this.challengeName = challengeName;
         this.testFile = testFile;
         this.testName = testName;
+        this.filePath = filePath;
+        this.fileContent = fileContent;
     }
 
     public String getChallengeName() {
         return challengeName;
     }
 
-    public void setChallengeName(String challengeName) {
-        this.challengeName = challengeName;
-    }
-
     public String getTestName() {
         return testName;
-    }
-
-    public void setTestName(String testName) {
-        this.testName = testName;
     }
 
     public String getTestFile() {
         return testFile;
     }
 
-    public void setTestFile(String testFile) {
-        this.testFile = testFile;
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getFileContent() {
+        return fileContent;
     }
 }
